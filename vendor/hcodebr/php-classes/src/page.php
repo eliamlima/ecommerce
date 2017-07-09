c7fc542278789a6fcc9352537ccf34a84d440961
Elia Lima <eliaml777@gmail.com>
2017-07-09 11:19:48 -0300

    erro http post no teste de login com classes User e Model

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -7,6 +7,8 @@ use Rain\Tpl;
 class Page {
 	private $tpl;
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,	
 		"data"=>[]
 	];
 	private $options = [];
@@ -27,7 +29,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true)	$this->tpl->draw("header");
 
 	}
 
@@ -44,7 +46,7 @@ class Page {
 	}
 
 	public function __destruct(){
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true)	$this->tpl->draw("footer");
 
 	}
 
